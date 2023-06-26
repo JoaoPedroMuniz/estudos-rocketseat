77c5d3d4837b0832ae1b90cd13dea0ca6dda1098
João Pedro <joaopedromuniz56@gmail.com>
2023-06-26 20:31:57 -0300

    Criação de exercícios de JS
    Função, console.log, soma..

---
+++ javascript/exercícios/ex01.js
@@ -0,0 +1,9 @@
+let/*Or var*/ BoasVindas = 'Fala dev!';
+console.log('1°= ' + BoasVindas);
+BoasVindas = 'Fala dev, beleza?!'
+console.log('2°= ' + BoasVindas)
+const SerHumano = true;
+console.log(SerHumano);
+
+/*SerHumano = false; Não pode fazer isso, const != de let/var, tem que manter o valor inicial*/
+console.log('Ou seja,' + SerHumano);
---
+++ javascript/exercícios/ex02.js
@@ -0,0 +1,18 @@
+
+/*Function of string for something caracters*/
+
+function saudacao(nome){
+    console.log("Olá " + nome + " Como você está?");
+}
+
+saudacao('Pedro');
+
+/*Função de soma*/
+
+function soma(a, b){
+    return console.log(a + b)
+}
+
+
+console.log('Resultando da soma abaixo '), (soma(6, 4))
+
